95d20838b9b56ad8a4938e5981a1232c07d9e1c7
Thomasg443 <142933037+Thomasg443@users.noreply.github.com>
2024-11-23 12:37:32 -0500

    Support JS and HTML work

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-document.addEventListener("DOMContentLoaded", getWeather)
+document.addEventListener("DOMContentLoaded", getWeather);
+
+let question1 = document.getElementById("q1");
+let question2 = document.getElementById("q2");
+let question3 = document.getElementById("q3");
+let question4 = document.getElementById("q4");
+
+question1.addEventListener("click", unhide);
+
+
+function unhide() {
+    let answer1 = document.getElementById("answer1")
+    if (answer1.hidden){
+        answer1.hidden = false;
+    }
+    else {
+        answer1.hidden = false;
+    }
+}
 
 function getWeather() {
     let endpoint = "https://api.weatherapi.com/v1/current.json";
@@ -17,13 +35,14 @@ function responseReceivedHandler() {
     let weatherInfo = document.getElementById("weather");
     if (this.status === 200) {
         weatherInfo.innerHTML = 
-            "<p>Updated: " + this.response.current.last_updated + "</p>" +
-            "<p>Current temperature: " + this.response.current.temp_f + " &deg;F</p>" + 
-            "<p>Feels like: " + this.response.current.feelslike_f + " &deg;F</p>" + 
-            "<p>Condition: " + this.response.current.condition.text + "</p>" +  
-            "<p>Humidity: " + this.response.current.humidity + "%</p>"; 
+            "<p>Updated: <span class=\"r\">" + this.response.current.last_updated + "</span></p>" +
+            "<p>Temperature: <span class=\"r\">"  + this.response.current.temp_f + "&deg;F</span></p>" + 
+            "<p>Feels like: <span class=\"r\">" + this.response.current.feelslike_f + "&deg;F</span></p>" + 
+            "<p>Condition: <span class=\"r\">" + this.response.current.condition.text + "</span></p>" +  
+            "<p>Humidity: <span class=\"r\">" + this.response.current.humidity + "</span></p>"; 
     } else {
         weatherInfo.innerHTML = "Weather data unavailable.";
     }
 }
+
 
